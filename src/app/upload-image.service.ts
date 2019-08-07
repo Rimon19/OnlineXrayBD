fbd687c1028277b51b7b8938916f7b604d423b2e
Rimon19 <mominur.hossain@outlook.com>
2019-08-07 10:33:14 +0600

    uploadImagePageFinallyDone

--- src/app/upload-image.service.ts
+++ src/app/upload-image.service.ts
@@ -1,13 +1,49 @@
+import { AngularFireDatabase } from '@angular/fire/database';
+import { AngularFireStorage, AngularFireUploadTask } from '@angular/fire/storage';
 import { UploadImage } from './Model/upload-image';
 import { Injectable } from '@angular/core';
 import * as firebase from 'firebase/app';
+import { Observable } from 'rxjs';
+import { tap, finalize } from 'rxjs/operators';
+import { async } from 'q';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UploadImageService {
   uImage=new UploadImage;
-  constructor() { }
+
+  task:AngularFireUploadTask;
+  percentage:Observable<number>;
+  snapshot:Observable<any>;
+  downloadURL:string;
+  constructor(private db: AngularFireDatabase, private storage:AngularFireStorage) { }
+
+  startUpLoad(uploadImage: UploadImage){
+    const path=`uploadImage/${Date.now()}_${uploadImage.imageUrlFile.name}`;
+    const ref=this.storage.ref(path);
+    
+    this.task=this.storage.upload(path,uploadImage.imageUrlFile);
+ 
+
+    this.percentage=this.task.percentageChanges();
+ 
+   
+    this.snapshot=this.task.snapshotChanges().pipe(
+      finalize(async()=>{
+        await ref.getDownloadURL().toPromise().then(t=>{
+
+          uploadImage.imageUrl=t;
+          this.db.list(`uploadImage/`).push(uploadImage);
+        });
+
+        
+      
+      }),
+    );
+        
+    this.snapshot.subscribe(d=>{})
+  }
 
   pushUpload(uploadImage: UploadImage) {
     let storageRef = firebase.storage().ref();
