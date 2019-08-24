331d82adbed7e02317a34dc0fa90e839544a8241
Rimon19 <mominur.hossain@outlook.com>
2019-08-24 11:26:32 +0600

    addedReport

--- src/app/print-report/print-report.component.ts
+++ src/app/print-report/print-report.component.ts
@@ -2,6 +2,8 @@ import { UploadImageService } from './../upload-image.service';
 import { ActivatedRoute } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 import { UploadImage } from '../Model/upload-image';
+import * as jspdf from 'jspdf';  
+import html2canvas from 'html2canvas';  
 
 @Component({
   selector: 'app-print-report',
@@ -29,4 +31,23 @@ export class PrintReportComponent implements OnInit {
        
    }
 
+
+captureScreen() {  
+    var data = document.getElementById('contentToConvert');  
+    html2canvas(data).then(canvas => {  
+      // Few necessary setting options  
+      var imgWidth = 208;   
+      var pageHeight = 295;    
+      var imgHeight = canvas.height * imgWidth / canvas.width;  
+      var heightLeft = imgHeight;  
+  
+      const contentDataURL = canvas.toDataURL('image/png')  
+      let pdf = new jspdf('p', 'mm', 'a4'); // A4 size page of PDF  
+      var position = 0;  
+      pdf.addImage(contentDataURL, 'PNG', 0, position, imgWidth, imgHeight)  
+      pdf.save('MYPdf.pdf'); // Generated PDF   
+    });  
+  }  
+
+  
 }
