8b3aa322f3b491fad8e863e2eb8f658ee591a7fa
Rimon19 <mominur.hossain@outlook.com>
2019-09-07 20:23:16 +0600

    totalUploadHistryForUser

--- src/app/totaluploads-component/totaluploads-component.component.ts
+++ src/app/totaluploads-component/totaluploads-component.component.ts
@@ -13,8 +13,11 @@ import { UploadImage } from '../Model/upload-image';
 export class TotaluploadsComponentComponent implements OnInit {
   subscription: Subscription;
   appUser:AppUser[];
+  appUserOfDoctor=[];
   uploadedImagesInfos:UploadImage[];
+  uploadedImagesInfosForDoctors=[]
   calculateUploadImage=[];
+  calculateUploadImageForDoctor=[];
   constructor(private uploadeImageService:UploadImageService,
     private userServices:UserService) { }
 
@@ -22,73 +25,74 @@ export class TotaluploadsComponentComponent implements OnInit {
     var x = this.userServices.getAllUsers();   
     this.subscription= x.snapshotChanges().subscribe(item => {
       this.appUser = [];
-      item.forEach(element => {
-        var y = element.payload.toJSON();
-        y["key"] = element.key;
-      
-       if(y['isUser']==true) {
-        this.appUser.push(y as AppUser);  
-        
-       }                 
-      });  
-      
-
-     
-      this.appUser.forEach(element => {
-
-        var x = this.uploadeImageService.getUploadImageByUserId(element.key);
-            this.subscription= x.subscribe(item => {
-
-                        this.uploadedImagesInfos = [];
+            item.forEach(element => {
+              var y = element.payload.toJSON();
+              y["key"] = element.key;
+            
+              if(y['isUser']==true) {
+                this.appUser.push(y as AppUser);  
+                
+              }     
+                
                       
+            });  
+            
 
-                        item.forEach(element => {
-                          var y = element.payload.toJSON();
-                          y["key"] = element.key;  
+                this.appUser.forEach(element => {
 
-                          this.uploadedImagesInfos.push(y as UploadImage);                        
-                        
-                        });
+                  var x = this.uploadeImageService.getUploadImageByUserId(element.key);
+                      this.subscription= x.subscribe(item => {
+
+                                  this.uploadedImagesInfos = [];
+                                
+
+                                  item.forEach(element => {
+                                    var y = element.payload.toJSON();
+                                    y["key"] = element.key;  
+
+                                    this.uploadedImagesInfos.push(y as UploadImage);                        
+                                  
+                                  });
 
 
-                        var dateObj = new Date();
-                        var thisMonth = dateObj.getUTCMonth() + 1;
-                        var previousMonth=dateObj.getUTCMonth();
-                        var day = dateObj.getUTCDate();
-                        let  countThisMonth=0;
-                        let countPrevMonth=0;
-                      
-                        var c=[];
-                        this.uploadedImagesInfos.forEach(element => {
-                          var entryDatObj=new Date(element.entryDate);
-                          var month=entryDatObj.getUTCMonth() + 1;
+                                  var dateObj = new Date();
+                                  var thisMonth = dateObj.getUTCMonth() + 1;
+                                  var previousMonth=dateObj.getUTCMonth();
+                                  var day = dateObj.getUTCDate();
+                                  let  countThisMonth=0;
+                                  let countPrevMonth=0;
+                                
+                                  var c=[];
+                                  this.uploadedImagesInfos.forEach(element => {
+                                    var entryDatObj=new Date(element.entryDate);
+                                    var month=entryDatObj.getUTCMonth() + 1;
 
-                          if(thisMonth==month){
-                            countThisMonth++;
-                        }
-                        
-                        c["thisMonth"]=countThisMonth;
-                        if(previousMonth==month){
-                            countPrevMonth++;
-                        }
-                        c["lasMonth"]=countPrevMonth;
+                                    if(thisMonth==month){
+                                      countThisMonth++;
+                                  }
+                                  
+                                  c["thisMonth"]=countThisMonth;
+                                  if(previousMonth==month){
+                                      countPrevMonth++;
+                                  }
+                                  c["lasMonth"]=countPrevMonth;
 
-                       
-                          
+                                
+                                    
 
-                        });
-                        c['total']=this.uploadedImagesInfos.length;
-                        c['userName']=element.inistituteName;                        
-                        this.calculateUploadImage.push(c); 
-                        
-                        
+                                  });
+                                  c['total']=this.uploadedImagesInfos.length;
+                                  c['inistituteName']=element.inistituteName;                        
+                                  this.calculateUploadImage.push(c); 
+                                  
+                                  
 
-                  }); 
+                            }); 
 
-            });
-      });
+                      });
 
-      console.log(this.calculateUploadImage);
+                  
+    });
 
       
  }
